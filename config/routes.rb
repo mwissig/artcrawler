7b3e04fd7daa9beb39a3369483109a4ccdef7e11
Milo Wissig <mwissig@gmail.com>
2018-09-23 14:32:40 -0400

    added users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  get 'sessions/new'
+  get 'users/new'
+  get 'users/index'
+  get 'users/edit'
+  get 'users/show'
+ root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
