f0b8208ffb5a8ba18105f1a45982717f3a8c199e
Milo Wissig <mwissig@gmail.com>
2018-10-03 10:00:34 -0400

    seed 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# 50.times do
-#   i = rand(0..2)
-#   purp = ["Visitor", "Artist", "Organizer"]
-#   User.create(
-#     email: Faker::Internet.unique.email,
-#     primary_use: purp[i],
-#     password: "password"
-#   )
-# end
-#
+100.times do
+  i = rand(0..2)
+  purp = ["Visitor", "Artist", "Organizer"]
+  cities = ["Brooklyn", "Queens", "New York", "Long Island City"]
+  c = rand(0..3)
+  User.create(
+    email: Faker::Internet.unique.email,
+    primary_use: purp[i],
+    password: "password",
+    :city => cities[c],
+    :state => "NY"
+  )
+end
+
 # User.all.each do |u|
 #   media = ["Architecture", "Book Arts", "Ceramics", "Conceptual Art", "Craft", "Dance", "Design", "Drawing", "Fashion", "Fiber", "Film and Video", "Illustration", "Installation", "Jewelry", "Mixed Media", "Music", "New Media", "Painting", "Performance", "Photography", "Printmaking", "Relational Art", "Sculpture", "Sound", "Street Art"]
-#   cities = ["Brooklyn", "Queens", "New York", "Long Island City"]
 #   y = rand(0..24)
 #   z = rand(0..24)
-#   c = rand(0..3)
 #   @profile = u.build_profile(
 #         :name => Faker::Name.name,
 #         :portfolio => "http://www.mwissig.com",
@@ -30,15 +32,13 @@
 #         :statement => Faker::Lorem.paragraphs(paragraph_count = 2, supplemental = false),
 #         :primary_medium => media[y],
 #         :secondary_medium => media[z],
-#         :city => cities[c],
-#         :state => "NY"
 #   )
 #   @profile.user_id = u.id,
 #     @profile.save!
 # end
 #
 # 50.times do
-#   x = rand(6..97)
+#   x = rand(1..100)
 #   media = ["Architecture", "Book Arts", "Ceramics", "Conceptual Art", "Craft", "Dance", "Design", "Drawing", "Fashion", "Fiber", "Film and Video", "Illustration", "Installation", "Jewelry", "Mixed Media", "Music", "New Media", "Painting", "Performance", "Photography", "Printmaking", "Relational Art", "Sculpture", "Sound", "Street Art"]
 #   y = rand(0..24)
 #   z = rand(0..24)
@@ -53,8 +53,8 @@
 #   )
 # end
 
-Location.create(
-  event_id: 7,
-  user_id: 1,
-  address:
-)
+# Location.create(
+#   event_id: 7,
+#   user_id: 1,
+#   address:
+# )
